1e9e5669d167739eab1e4dd809424e7bd2ed8521
Marco van den Oever <marco@flyingpie.nl>
2025-02-22 14:48:12 +0100

    Workspace

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using System.Diagnostics;
 using System.Runtime.InteropServices;
+using System.Security;
+using System.Text;
 
 namespace BllngBllng;
 
@@ -7,59 +9,180 @@ public static class Program
 {
 	public static void Main(string[] args)
 	{
-		const string WtProcName = "WindowsTerminal";
+		Console.WriteLine("""
+			Usage:
+				dotnet run <process name> <show|hide>
+			E.g.:
+				dotnet run WindowsTerminal show
+				dotnet run WindowsTerminal hide
+			""");
 
-		var isOn = args.Contains("show", StringComparer.OrdinalIgnoreCase);
+		if (args.Length < 2)
+		{
+			return;
+		}
+
+		var procName = args[0];
+		var isVisible = args[1].Equals("show", StringComparison.OrdinalIgnoreCase);
 
 		var processes = Process
 			.GetProcesses()
-			.Where(p => p.ProcessName.Equals(WtProcName, StringComparison.OrdinalIgnoreCase))
+			.Where(p => p.ProcessName.Equals(procName, StringComparison.OrdinalIgnoreCase))
 			.ToList();
 
 		if (processes.Count == 0)
 		{
-			Console.WriteLine($"Did not find any processes with name '{WtProcName}', are you sure it's running?");
+			Console.WriteLine($"Did not find any processes with name '{procName}', are you sure it's running?");
 			return;
 		}
 
+		var taskbar = (ITaskbarList)new CTaskbarList();
+		taskbar.HrInit();
+
 		foreach (var process in processes)
 		{
-			SetIconVisible(process, isOn);
+			var windowInfos = GetWindowInfos(process);
+
+			foreach (var windowInfo in windowInfos)
+			{
+				if (isVisible)
+				{
+					taskbar.AddTab(windowInfo.Handle);
+				}
+				else
+				{
+					taskbar.DeleteTab(windowInfo.Handle);
+				}
+			}
 		}
 	}
 
-	public static void SetIconVisible(Process process, bool isVisible)
+	public static IEnumerable<WindowInfo> GetWindowInfos(Process process)
 	{
-		var handle = process.MainWindowHandle;
+		foreach (var thread in process.Threads.OfType<ProcessThread>())
+		{
+			var windowHandles = User32.EnumThreadWindowsGet(thread.Id, null).ToList();
 
-		Console.WriteLine($"Setting taskbar visibility icon {(isVisible ? "VISIBLE" : "HIDDEN")} for process with pid '{process.Id}' and window handle '{process.MainWindowHandle}'");
+			if (windowHandles.Count == 0)
+			{
+				continue;
+			}
 
-		// Get current window properties
-		var props = User32.GetWindowLong(handle, User32.GWLEXSTYLE);
+			foreach (var windowHandle in windowHandles)
+			{
+				var windowFlags_style = User32.GetWindowLong(windowHandle, User32.GWL_STYLE);
+				var windowText = User32.GetWindowText(windowHandle);
 
-		Console.WriteLine($"Current window props for window with pid '{process.Id}': {props:x8}");
+				var isVisible = (windowFlags_style & User32.WS_VISIBLE) == User32.WS_VISIBLE;
 
-		var newProps = isVisible
-			// Show
-			? (props | User32.WSEXTOOLWINDOW) & User32.WSEXAPPWINDOW
-			// Hide
-			: (props | User32.WSEXTOOLWINDOW) & ~User32.WSEXAPPWINDOW;
+				// TODO: Maybe also exclude child windows?
+				var isChild = (windowFlags_style & User32.WS_CHILDWINDOW);
 
-		Console.WriteLine($"SetWindowLong(hWnd:{handle:x8}, nIndex{User32.GWLEXSTYLE:x8}, dwNewLong{newProps:x8})");
+				if (!isVisible)
+				{
+					continue;
+				}
 
-		User32.SetWindowLong(handle, User32.GWLEXSTYLE, newProps);
+				yield return new WindowInfo()
+				{
+					Handle = windowHandle,
+					Caption = windowText,
+				};
+			}
+		}
+	}
+
+	public class WindowInfo
+	{
+		public nint Handle { get; set; }
+
+		public string? Caption { get; set; }
 	}
 }
 
 public static class User32
 {
-	public const int GWLEXSTYLE = -20;
-	public const int WSEXAPPWINDOW = 0x00040000;
-	public const int WSEXTOOLWINDOW = 0x00000080;
+	public const int GWL_STYLE = -16;
+
+	public const long WS_CHILDWINDOW = 0x40000000L;
+	public const long WS_VISIBLE = 0x10000000L;
+
+	public delegate bool EnumWindowsProc(IntPtr hWnd, IntPtr lParam);
+
+	[DllImport("user32.dll")]
+	public static extern bool EnumThreadWindows(IntPtr dwThreadId, EnumWindowsProc enumProc, IntPtr lParam);
+
+	public static IEnumerable<IntPtr> EnumThreadWindowsGet(IntPtr dwThreadId, EnumWindowsProc filter)
+	{
+		var found = IntPtr.Zero;
+		var windows = new List<IntPtr>();
+
+		EnumThreadWindows(dwThreadId, delegate (IntPtr wnd, IntPtr param) { windows.Add(wnd); return true; }, IntPtr.Zero);
+
+		return windows;
+	}
 
 	[DllImport("user32.dll", SetLastError = true)]
 	public static extern int GetWindowLong(nint hWnd, int nIndex);
 
-	[DllImport("user32.dll", SetLastError = true)]
-	public static extern int SetWindowLong(nint hWnd, int nIndex, int dwNewLong);
+	[DllImport("user32.dll", CharSet = CharSet.Unicode)]
+	private static extern int GetWindowText(IntPtr hWnd, StringBuilder strText, int maxCount);
+
+	[DllImport("user32.dll", CharSet = CharSet.Unicode)]
+	private static extern int GetWindowTextLength(IntPtr hWnd);
+
+	public static string GetWindowText(IntPtr hWnd)
+	{
+		int size = GetWindowTextLength(hWnd);
+		if (size > 0)
+		{
+			var builder = new StringBuilder(size + 1);
+			GetWindowText(hWnd, builder, builder.Capacity);
+			return builder.ToString();
+		}
+
+		return string.Empty;
+	}
+}
+
+//
+// Summary:
+//     Class interface for ITaskbarList and derivatives.
+[ComImport]
+[Guid("56FDF344-FD6D-11d0-958A-006097C9A090")]
+[ClassInterface(ClassInterfaceType.None)]
+public class CTaskbarList
+{
+}
+
+[ComImport]
+[SuppressUnmanagedCodeSecurity]
+[InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+[Guid("56FDF342-FD6D-11d0-958A-006097C9A090")]
+[CoClass(typeof(CTaskbarList))]
+public interface ITaskbarList
+{
+	//
+	// Summary:
+	//     Initializes the taskbar list object. This method must be called before any other
+	//     ITaskbarList methods can be called.
+	void HrInit();
+
+	//
+	// Summary:
+	//     Adds an item to the taskbar.
+	//
+	// Parameters:
+	//   hwnd:
+	//     A handle to the window to be added to the taskbar.
+	void AddTab(IntPtr hwnd);
+
+	//
+	// Summary:
+	//     Deletes an item from the taskbar.
+	//
+	// Parameters:
+	//   hwnd:
+	//     A handle to the window to be deleted from the taskbar.
+	void DeleteTab(IntPtr hwnd);
 }
